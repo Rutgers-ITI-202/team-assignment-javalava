0f51b6375360957eb8e03d460c59ae2c9a63dfa4
ChrisMDC <chrisagichua@gmail.com>
2017-02-11 18:31:53 -0500

    Initial updates 2/11/17
    1. Main method updated with start screen function
    2. Adventurer class updated with view menu function

--- src/adventure/Adventurer.java
+++ src/adventure/Adventurer.java
@@ -11,6 +11,40 @@ An adventurer can:
 
 package adventure;
 
+import java.util.Scanner;
+
 public class Adventurer {
 
+/**
+ * Allows Player Access to Menu to view inventory, quit game, exit menu
+ * More functionalities to come
+ * @author ChrisMDC
+ * @since 2/11/2017
+ *
+ */	
+public static void MENU() {
+	boolean menuLoop = false;
+	System.out.println("<<OPEN MENU>> \rView Inventory: A \rQuit Game: B \rExit Menu: C");
+	while (menuLoop = false){
+	Scanner menu = new Scanner(System.in);
+	String option = menu.nextLine();			
+		if (option.equalsIgnoreCase("A") == true){
+			// METHOD TO VIEW INVENTORY HERE
+			menuLoop = true;
+		} else if (option.equalsIgnoreCase("B") == true) {
+			// METHOD TO QUIT GAME HERE
+			menuLoop = true;
+		} else if (option.equalsIgnoreCase("C") == true){
+			System.out.println("<<CLOSED MENU>>");
+			// return to game
+			menuLoop = true;
+		} else {
+			System.out.println("Your entry " + option + ", is not a viable option. Enter again.");
+		}	
+}		
+	
 }
+
+	
+}
+
--- src/adventure/TextAdventure.java
+++ src/adventure/TextAdventure.java
@@ -6,11 +6,44 @@ This class:
 
 package adventure;
 
+import java.util.Scanner;
+
+/**
+ * Main method of text adventure
+ * @author ChrisMCD
+ * @since 2/11/17
+ *
+ */
+
 public class TextAdventure {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+System.out.println("GAME TITLE TBA");
+		
+startGame(); // start game sequence
 
+
+	}
+
+/**
+ * Method to boot the game
+ * @author: ChrisMDC
+ * @since: 2/11/17
+ */
+
+	public static void startGame() {
+		System.out.println("<<Press A to Start>>");
+		Scanner begin = new Scanner(System.in);
+		String A = begin.nextLine();
+			if (A.equalsIgnoreCase("a") == true){
+				System.out.println("Game loading...");
+							// INSERT METHOD TO INTRODUCE ROOM AND PLAYER
+			} else if (A.equalsIgnoreCase("a") == false){
+				System.out.println("You entered " + A + ", which did not work.");
+				startGame();
+			}
+		
 	}
 
 }
