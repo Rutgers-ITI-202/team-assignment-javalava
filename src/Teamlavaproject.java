1f713580029b4d024dbb68141436a6d6c9086343
clin815 <cl970@rutgers.edu>
2017-02-24 18:22:04 -0500

    hghghghg

---
+++ src/Teamlavaproject.java
@@ -0,0 +1,71 @@
+import java.util.*;
+public class Teamlavaproject {
+
+}
+
+
+/**
+ * TEST CODE GIt HUB PUSH
+ * 
+ * Intresting article I found about creating a room and direction: 
+ * http://stackoverflow.com/questions/41419661/text-adventure-interactive-fiction-in-java
+ */
+
+/* User is a child in the 1820's industrial  revolution child worker imprisoned 
+ * by his employer. Employer gives him clues to escape the factory and he has
+ * to find golden keys. Each of the  100 rooms contain a different theme and different
+ * ways to unlock the door.
+ */
+
+
+//Start game now
+
+//  " your lying on the floor of a concrete textile building"
+
+// obvious exit: metal door at the end of the hall
+//visible items : Looms, clothes, drawer.
+
+//------------------------------------------------>
+
+System.out.println.out()"Welcome to 1920's Industrial "
+		+ "revolution era. You are a child stuck in a factory by his maniactic employer. you must ESCAPE the Factory by obeying the "
+		+ " employer over the overhead speaker. "
+		+ " you are carrying a t shirt, watch, gum";
+
+Scanner scanner = new Scanner (System.in); 
+
+System.out.print (" What do you want to do? ");
+
+
+
+
+/* Incorrect statements when looking,  maybe "I see nothing special" when user types "look"
+ * User types incorrrect do "I don't know how what is  "   "  is " 
+ * 
+ */
+
+/* when user types in  " look in drawer" in Scanner. User finds a golden key. 
+ * User picks up Key, and than " go to door", after go to door.User types " use key"
+ * User succesffull finishes first room
+ * 
+ * due 2/24/17- make room, pick up and drop item
+ */
+
+
+
+
+
+
+//system.out.println() " The room is silent, the tinkering of looms echos in the factor room"";
+
+
+
+
+
+
+public void add (int item );
+
+{ 
+	
+	
+}
